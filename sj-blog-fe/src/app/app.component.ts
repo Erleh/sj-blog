535bf8b7f226a08c6ccb471f63cb1e3dd6977c5c
Eric Lee <eric.lee0051@gmail.com>
2024-10-15 19:39:25 -0700

    Added start for footer, working progressively on styling

--- sj-blog-fe/src/app/app.component.ts
+++ sj-blog-fe/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { PostlistComponent } from './postlist/postlist.component';
 import { TestbarComponent } from "./testbar/testbar.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, PostlistComponent, TestbarComponent],
+  imports: [RouterOutlet, NavbarComponent, PostlistComponent, TestbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
